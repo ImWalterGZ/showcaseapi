cab09dc8cc648acc69f0993700fc441da64e78d2
GZZZ <walter.gz@icloud.com>
2024-10-18 15:03:30 -0600

    Se agregó api para llenar posts placeholders

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,3 +1,21 @@
-export default function Postspage() {
-  return <div>Aqui mostramos los posts.</div>;
+const fetchPosts = () => {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json()
+  );
+};
+
+export default async function Postspage({ params }) {
+  const posts = await fetchPosts();
+  return (
+    <div className="px-40 py-10">
+      <section>
+        {posts.slice(0, 5).map((post) => (
+          <article key={post.id} className="my-5">
+            <h2 className="font-bold text-2xl">{post.title}</h2>
+            <p>{post.body}</p>
+          </article>
+        ))}
+      </section>
+    </div>
+  );
 }
