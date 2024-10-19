50131da7ea52a4242b1624a40ec2326de4cc126e
GZZZ <walter.gz@icloud.com>
2024-10-18 18:10:14 -0600

    Cambio en el page app

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
       <div className="w-full h-16 bg-neutral-600 flex flex-row px-2 py-2 justify-around items-center transition-all transform hover:-translate-y-2"></div>
       <div className="w-full h-16 bg-neutral-700 flex flex-row px-2 py-2 justify-around items-center transition-all transform hover:-translate-y-2"></div>
       <div className="w-full h-16 bg-neutral-800 flex flex-row px-2 py-2 justify-around items-center transition-all transform hover:-translate-y-2"></div>
-      <div className="w-full h-screen bg-neutral-900 flex flex-row px-2 py-2 justify-around items-start ">
-        <div className="w-80 h-80 relative rounded-xl drop-shadow-2xl mt-12 ">
+      <div className="w-full h-screen bg-neutral-900 flex flex-row px-2 py-2 justify-around  ">
+        <div className="w-80 h-80 relative rounded-xl drop-shadow-2xl mt-12 items-center align-middle ">
           <Image
             src="/gzzz.jpeg"
             alt="GZZZ"
